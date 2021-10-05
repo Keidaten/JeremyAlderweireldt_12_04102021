6d11f7286d18a53bf882485032f08a65a8b1a10a
Jey <jeremyalder@hotmail.fr>
2021-10-05 18:07:39 +0200

    starting with recharts

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 //Components
 import MainTitle from '../../components/MainTitle';
+import BiaxialBarChart from '../../components/BiaxialBarChart';
 
 function Home() {
 	return (
 		<main>
 			<MainTitle />
+			<BiaxialBarChart />
 		</main>
 	);
 }
